3b6e9eab45576ff7ea6827bf8e8748ca8bf6966c
Nelmeris <KufaevArtem@icloud.com>
2019-02-13 20:41:44 +0300

    The algorithm of drawing on points and passing through transformations is written Trying to create a modifiable array of shape points

--- Computer-Graphics/CoreTransform/CoreTransform.m
+++ Computer-Graphics/CoreTransform/CoreTransform.m
@@ -10,6 +10,7 @@
 
 @implementation CoreTransform
 
+// Перемножение матриц
 + (void) times: (Matrix)a andB: (Matrix)b andC: (Matrix)c {
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < M; j++) {
@@ -21,6 +22,7 @@
     }
 }
 
+// Перемножение матрицы на вектор
 + (void) timesMatVec: (Matrix)a andB: (Vector)b andC: (Vector)c {
     for (int i = 0; i < M; i++) {
         float skalaar = 0;
@@ -30,25 +32,30 @@
     }
 }
 
+// Копирование матрицы
 + (void) set: (Matrix)a andB: (Matrix)b {
     for (int i = 0; i < M; i++)
         for (int j = 0; j < M; j++)
             b[i][j] = a[i][j];
 }
 
+// Перевод точки в вектор
 + (void) point2vec: (NSPoint)a andB: (Vector)b {
     b[0] = a.x; b[1] = a.y; b[2] = 1;
 }
 
+// Перевод вектора в точку
 + (void) vec2point: (Vector)a andB: (NSPoint) b {
     b.x = ((float)a[0]) / a[2];
     b.y = ((float)a[1]) / a[2];
 }
 
+// Создание вектора
 + (void) makeHomogenVec: (float)x andY: (float)y andC: (Vector)c {
     c[0] = x; c[1] = y; c[2] = 1;
 }
 
+// Сброс матрицы
 + (void) unit: (Matrix)a {
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < M; j++) {
@@ -58,18 +65,21 @@
     }
 }
 
+// Трансформация перемещения
 + (void) move: (float)Tx andTy: (float)Ty andC: (Matrix)c {
     [CoreTransform unit:c];
     c[0][M - 1] = Tx;
     c[1][M - 1] = Ty;
 }
 
+// Трансформация поворота
 + (void) rotate: (float)phi andC: (Matrix)c {
     [CoreTransform unit:c];
     c[0][0] = cos(phi); c[0][1] = -sin(phi);
     c[1][0] = sin(phi); c[1][1] = cos(phi);
 }
 
+// Трансформация скалирования
 + (void) scale: (float)S andC: (Matrix)c {
     [CoreTransform unit:c];
     c[0][0] = S; c[1][1] = S;
--- Computer-Graphics/GraphicView/GraphicView.m
+++ Computer-Graphics/GraphicView/GraphicView.m
@@ -6,8 +6,10 @@
 //  Copyright © 2019 Artem Kufaev. All rights reserved.
 //
 
+// #import <Cocoa/Cocoa.h>
 #import "GraphicView.h"
 #import "CoreTransform.h"
+// #import <UIKit/UIKit.h>
 
 @implementation GraphicView
 
@@ -15,6 +17,9 @@
 {
     if ((self = [super initWithCoder:coder])) {
         points = [NSMutableArray array];
+        [CoreTransform unit:transform];
+        
+//        [points addObject:[NSValue valueWithPoint:CGPointMake(10, 10)]];
     }
     return self;
 }
@@ -25,29 +30,25 @@
     // Setting background
     NSBezierPath *background = [NSBezierPath bezierPath];
     [background appendBezierPathWithRect: NSMakeRect(0, 0, self.frame.size.width, self.frame.size.height)];
-    
+
     [[NSColor whiteColor] setFill];
     [background fill];
     [background stroke];
-    
+
     NSBezierPath *figure = [NSBezierPath bezierPath];
-    
-    Vector A;
-    NSPoint* point = (NSPoint*)CFBridgingRetain(points[points.count - 1]);
-    [CoreTransform point2vec: *point andB: A];
-    
-    Vector B;
-    [CoreTransform timesMatVec: transform andB: A andC: B];
-    
-    NSPoint newPoint;
-    [CoreTransform vec2point: B andB: newPoint];
-    
-    [figure moveToPoint: newPoint];
-    
-    for (int i = 0; i < points.count; i++) {
+
+    if (points.count != 0) {
+        NSInteger count = points.count;
+        NSInteger index = count - 1;
+        
+        NSValue *value = [points objectAtIndex: index];
+        CGPoint cgPoint;
+//        cgPoint = [value CGPointValue];
+        
+        NSPoint point = NSMakePoint(cgPoint.x, cgPoint.y);
+        // Установка начальной позиции фигуры на последней точке
         Vector A;
-        NSPoint* point = (NSPoint*)CFBridgingRetain(points[i]);
-        [CoreTransform point2vec: *point andB: A];
+        [CoreTransform point2vec: point andB: A];
         
         Vector B;
         [CoreTransform timesMatVec: transform andB: A andC: B];
@@ -55,9 +56,23 @@
         NSPoint newPoint;
         [CoreTransform vec2point: B andB: newPoint];
         
-        [figure lineToPoint: newPoint];
+        [figure moveToPoint: newPoint];
+        
+        // Черчение линии по всем точкам
+        for (int i = 0; i < points.count; i++) {
+            Vector A;
+            NSPoint* point = (NSPoint*)CFBridgingRetain(points[i]);
+            [CoreTransform point2vec: *point andB: A];
+            
+            Vector B;
+            [CoreTransform timesMatVec: transform andB: A andC: B];
+            
+            NSPoint newPoint;
+            [CoreTransform vec2point: B andB: newPoint];
+            
+            [figure lineToPoint: newPoint];
+        }
     }
-    
     
     [figure stroke];
 }
