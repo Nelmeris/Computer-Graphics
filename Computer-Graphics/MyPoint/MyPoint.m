d62f678d3bddad490d62f8993e0821157cb31c05
Nelmeris <KufaevArtem@icloud.com>
2019-02-14 12:31:45 +0300

    Created a point class

---
+++ Computer-Graphics/MyPoint/MyPoint.m
@@ -0,0 +1,20 @@
+//
+//  Point.m
+//  Computer-Graphics
+//
+//  Created by Artem Kufaev on 14/02/2019.
+//  Copyright © 2019 Artem Kufaev. All rights reserved.
+//
+
+#import "MyPoint.h"
+
+@implementation MyPoint
+
+- (id) init:(NSInteger)x andY:(NSInteger)y {
+    self = [super init];
+    self.x = x;
+    self.y = y;
+    return self;
+}
+
+@end
